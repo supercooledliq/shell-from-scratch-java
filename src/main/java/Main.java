78851dce8b642988a0930c45851edcbb66b46389
supercooledliq <tanmayeebahadursha@gmail.com>
2025-11-03 10:25:42 +0530

    exit Builtin

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        while (true) {
+        boolean i = true;
+        while (i == true) {
         System.out.print("$ ");
         Scanner sc=new Scanner(System.in);
         String command = sc.nextLine();
+        if(command != "exit 0")
         System.out.println(command+": command not found");
+        else
+        i = false;
         }
        
     }
