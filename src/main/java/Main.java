92269db78e30cb74cf2934dea02e26da682fc900
supercooledliq <tanmayeebahadursha@gmail.com>
2025-11-03 10:02:28 +0530

    printing error

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
         System.out.print("$ ");
+        Scanner sc=new Scanner(System.in);
+        String command = sc.nextLine();
+        System.out.println(command+": command not found");
     }
 }
