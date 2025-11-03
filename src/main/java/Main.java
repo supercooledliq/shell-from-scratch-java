31d992f314c88ae3960fe8b2d9d17e2a2d9e2449
supercooledliq <tanmayeebahadursha@gmail.com>
2025-11-03 10:18:20 +0530

    implementing REPL

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // TODO: Uncomment the code below to pass the first stage
+        while (true) {
         System.out.print("$ ");
         Scanner sc=new Scanner(System.in);
         String command = sc.nextLine();
         System.out.println(command+": command not found");
+        }
+       
     }
 }
