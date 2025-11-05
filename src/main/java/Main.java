92499ea0b56b511acd5c2c37c449577bebfeea79
supercooledliq <tanmayeebahadursha@gmail.com>
2025-11-05 07:10:26 +0530

    echo builtin

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,16 +4,25 @@ public class Main {
     public static void main(String[] args) throws Exception {
         boolean i = true;
         while (i) {
+            
         System.out.print("$ ");
         Scanner sc=new Scanner(System.in);
-        String command = sc.nextLine();
-        if(command.equals("exit 0"))
-        i = false;
-        else if(command.equals("echo"))
-        System.out.println(command);
-        else
-        System.out.println(command+": command not found");
+
+        String input = sc.nextLine();
+        String[] parts = input.split(" ", 2);
+        String command = parts[0];
+
+        switch(command)
+        {
+            case "echo": System.out.println(parts[1]);
+            break;
+
+            case "exit": System.exit(0);
+            break;
+
+            default: System.out.println(command+": command not found");
         }
+    }
        
     }
 }
